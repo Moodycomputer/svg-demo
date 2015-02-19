f5dfa7ece21a916ad69fd01394f7cfb5e414847f
Christopher Moody <terracomma@gmail.com>
2015-02-18 20:37:17 -0600

    changed from less to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 	path = require('path'),
 	watch = require('gulp-watch'),
-	less = require('gulp-less'),
+	sass = require('gulp-sass'),
 	connect = require('gulp-connect');
 
 // A simple hhtp server
@@ -12,18 +12,18 @@ gulp.task('connect', function() {
 });
 
 // Processes styles.less and spits out styles.css
-gulp.task('less', function () {
-	gulp.src('./public/styles/styles.less')
-		.pipe(less())
+gulp.task('sass', function () {
+	gulp.src('./public/styles/master.scss')
+		.pipe(sass())
 		.pipe(gulp.dest('./public/styles/'));
 });
 
 // Watches the less files and reprocresses styles.less upon change
 gulp.task('watch', function(){
-	watch('./public/styles/*.less', function(){
-		gulp.start('less');
+	watch('./public/styles/*.scss', function(){
+		gulp.start('sass');
 	})
 });
 
 
-gulp.task('serve', ['less', 'connect', 'watch']);
+gulp.task('serve', ['sass', 'connect', 'watch']);
